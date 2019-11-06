c7a9caac6e4f4215fde5200ae62f7a88702b36f7
s111795 <mark.patricio@iag.com.au>
2019-11-06 15:18:22 +1100

    Updates

---
+++ precache-manifest.0054cb3e56a7eed1f0e66f724c47d53f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f8421c83489853a808f6e040d4eb4c",
+    "url": "/pptxtract-hymns/index.html"
+  },
+  {
+    "revision": "b1103b11777bbf8b8e96",
+    "url": "/pptxtract-hymns/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "4d7deef61ad4863a0181",
+    "url": "/pptxtract-hymns/static/css/main.7edca76d.chunk.css"
+  },
+  {
+    "revision": "b1103b11777bbf8b8e96",
+    "url": "/pptxtract-hymns/static/js/2.e2b7324b.chunk.js"
+  },
+  {
+    "revision": "4d7deef61ad4863a0181",
+    "url": "/pptxtract-hymns/static/js/main.453ff696.chunk.js"
+  },
+  {
+    "revision": "a1edbac6f0d10d7ea8ab",
+    "url": "/pptxtract-hymns/static/js/runtime-main.dcef3d7b.js"
+  }
+]);
